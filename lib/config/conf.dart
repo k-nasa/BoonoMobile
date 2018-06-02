0689ca2c78ad67448ed8d759a5bbd0c8dd58d54d
k-nasa <htilcs1115@gmail.com>
2018-06-02 22:44:19 +0900

    add config class

---
+++ lib/config/conf.dart
@@ -0,0 +1,35 @@
+import 'package:http/http.dart' as http;
+import 'package:boono_mobile/config/db_manager.dart';
+import 'package:boono_mobile/route/api_routes.dart';
+
+final SEARVER_URL = 'http://e0457500.ngrok.io';
+
+class Config {
+  final DBManager dbManager = new DBManager();
+  String token;
+
+  //アプリを開いたときにcall
+  init() async {
+    await dbManager.openDB();
+    
+    if (await setToken() == null) {
+      await generateToken();
+    }
+    setToken();
+  }
+
+  setToken() async {
+    List<Map> config =  await dbManager.database.rawQuery("select * from config");
+    if (config.isEmpty) return null;
+
+    token = config.first['token'];
+
+    return config.first['token'];
+  }
+
+  generateToken() async {
+    var res = await http.post(UserCreateURL);
+    print(res.body);
+    dbManager.database.insert('config', { 'token': res.body });
+  }
+}
