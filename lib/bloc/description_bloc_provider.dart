92367d9bb4ecd8de725aea698257b1e6a81f4c72
k-nasa <htilcs1115@gmail.com>
2018-06-15 17:40:00 +0900

    add/ description_bloc_providerのコンストラクタを追加

--- lib/bloc/description_bloc_provider.dart
+++ lib/bloc/description_bloc_provider.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/widgets.dart';
 
 class DescriptionBlocProvider extends InheritedWidget {
+  final DescriptionBloc descriptionBloc;
+
+  DescriptionBlocProvider({
+    Key key,
+    Widget child,
+    DescriptionBloc descriptionBloc,
+}) : descriptionBloc  = descriptionBloc ?? DescriptionBloc(),
+  super(key: key, child: child)
+
   @override
   bool updateShouldNotify(InheritedWidget oldWidget) {
     // TODO: implement updateShouldNotify
