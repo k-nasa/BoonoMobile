2e46f59d228c91157cf25c777765ab4fdaa1d0af
k-nasa <htilcs1115@gmail.com>
2018-06-15 21:57:46 +0900

    add/ DescriptionBlocをどこからでも参照できるようにProviderでwrapする

--- lib/page/add_description_list.dart
+++ lib/page/add_description_list.dart
@@ -1,12 +1,15 @@
+import 'package:meta/meta.dart';
+
 import 'package:flutter/material.dart';
 import 'package:boono_mobile/model/subscription_item.dart';
 import 'package:boono_mobile/page/widget/type_select_field.dart';
+import 'package:boono_mobile/bloc/description_bloc_provider.dart';
 
 class AddSubscriptionItemPage extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return DescriptionBlocProvider(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisSize: MainAxisSize.max,
