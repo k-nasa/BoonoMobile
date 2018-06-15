97c5d05409013e57a637005ace680e366a4fc555
k-nasa <htilcs1115@gmail.com>
2018-06-15 23:55:58 +0900

    add/ subscriptionBlocにキーボードフォーカス時の処理を追加

--- lib/bloc/description_bloc.dart
+++ lib/bloc/description_bloc.dart
@@ -13,6 +13,11 @@ class SetContent {
   SetContent(this.content);
 }
 
+class ShowSelectField {
+  final bool isShow;
+  ShowSelectField(this.isShow);
+}
+
 class SubscriptionBloc{
   String content;
   String type = 'TitleItem';
@@ -23,8 +28,14 @@ class SubscriptionBloc{
   final StreamController<SetContent> _setContentController = StreamController<SetContent>();
   Sink<SetContent> get setContent => _setContentController.sink;
 
+  final StreamController<ShowSelectField> _showSelectFieldController = StreamController<ShowSelectField>();
+  Sink<ShowSelectField> get showField => _showSelectFieldController.sink;
+
   final BehaviorSubject<String> _type = BehaviorSubject<String>(seedValue: 'TitleItem');
   Stream<String> get typeString => _type.stream;
+
+  final BehaviorSubject<bool> _showSelectField = BehaviorSubject<bool>(seedValue: false);
+  Stream<bool> get showSelectField => _showSelectField.stream;
 
   final BehaviorSubject<String> _content = BehaviorSubject<String>(seedValue: '');
   Stream<String> get contentString => _content.stream;
@@ -33,6 +44,7 @@ class SubscriptionBloc{
   SubscriptionBloc() {
     _typeChangeController.stream.listen(_typeChange);
     _setContentController.stream.listen(_setContent);
+    _showSelectFieldController.stream.listen(_showSelect);
   }
 
   void _typeChange(TypeChange typeChange) {
@@ -41,6 +53,10 @@ class SubscriptionBloc{
   }
 
   void _setContent(SetContent setContent) { content = setContent.content; }
+
+  void _showSelect(ShowSelectField showSelectField) {
+    _showSelectField.add(showSelectField.isShow);
+  }
 
   Future<bool> subscriptionSave() async {
     SubscriptionItem subItem = SubscriptionItem(content: content, type: type);
