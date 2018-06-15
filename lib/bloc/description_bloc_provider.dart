a2ea626ab9167104e862049d9fe3083c71c82d9a
k-nasa <htilcs1115@gmail.com>
2018-06-15 17:31:55 +0900

    add/ DescriptionBlocのproviderファイルを追加

---
+++ lib/bloc/description_bloc_provider.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/widgets.dart';
+
+class DescriptionBlocProvider extends InheritedWidget {
+  @override
+  bool updateShouldNotify(InheritedWidget oldWidget) {
+    // TODO: implement updateShouldNotify
+  }
+}
