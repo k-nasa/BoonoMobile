2b8fb353afd67f1858d1b3709640fe29fe7e578a
k-nasa <htilcs1115@gmail.com>
2018-06-16 13:21:55 +0900

    add url

--- lib/route/api_routes.dart
+++ lib/route/api_routes.dart
@@ -1,5 +1,6 @@
 const String API_URL = 'http://ffb28d3b.ngrok.io';
 
 const String UserCreateURL = API_URL + '/users';
- const String SubscriptionCreateURL = API_URL + '/subscription_items';
- const String SubscriptionsURL = API_URL + '/subscription_items';
+const String SubscriptionCreateURL = API_URL + '/subscription_items';
+const String SubscriptionDeleteURL = API_URL + '/subscription_items';
+const String SubscriptionsURL = API_URL + '/subscription_items';
