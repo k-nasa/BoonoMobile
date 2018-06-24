741d505c1672ea35eb1c5a342152b2145b931a28
k-nasa <htilcs1115@gmail.com>
2018-06-24 11:47:35 +0900

    fix/ firebase_messagingのバージョン指定 + url

--- lib/route/api_routes.dart
+++ lib/route/api_routes.dart
@@ -1,6 +1,7 @@
-const String API_URL = 'https://boono.herokuapp.com';
+const String API_URL = 'http://b5d897ee.ngrok.io';
 
 const String UserCreateURL = API_URL + '/users';
+const String UserUpdateURL = API_URL + '/users';
 const String SubscriptionCreateURL = API_URL + '/subscription_items';
 const String SubscriptionDeleteURL = API_URL + '/subscription_items';
 const String SubscriptionsURL = API_URL + '/subscription_items';
