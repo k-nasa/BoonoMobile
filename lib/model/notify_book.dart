f5bbe614a0bf76fc97709b2c4c51179c1b9aa331
k-nasa <htilcs1115@gmail.com>
2018-06-24 15:27:35 +0900

    add model NotifyBook

---
+++ lib/model/notify_book.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:boono_mobile/route/api_routes.dart';
+import 'package:boono_mobile/config/db_manager.dart';
+
+class NotifyBook {
+  int id;
+  String title;
+  String author;
+  String publish_date;
+  String image_url;
+
+  NotifyBook({this.id, this.title, this.author, this.publish_date, this.image_url});
+
+  static Future<List> all() async {
+    DBManager db = new DBManager();
+    String userToken = await db.fetchUserToken();
+
+    final http.Response res = await http.get(
+      NotifyBookURL + '/$userToken',
+    );
+
+    final List jsonArray = json.decode(res.body);
+    List<NotifyBook> nBooks = [];
+
+    for(var nBook in jsonArray) {
+      nBooks.add(
+        new NotifyBook(
+          id: nBook['id'],
+          title:  nBook['title'],
+          author: nBook['author'],
+          image_url: nBook['image_url'],
+          publish_date: nBook['publish_date'],
+        )
+      );
+    }
+
+    return nBooks;
+  }
+}
