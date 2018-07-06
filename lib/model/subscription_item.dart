4a297b881e82dd1b279c8345e4e055a6e4d36b76
k-nasa <htilcs1115@gmail.com>
2018-07-06 12:38:53 +0900

    subItemModelの既存のメソッドを削除

--- lib/model/subscription_item.dart
+++ lib/model/subscription_item.dart
@@ -12,62 +12,11 @@ class SubscriptionItem {
   SubscriptionItem({this.content, this.type, this.id});
 
   Future<bool> save() async {
-    DBManager db = new DBManager();
-    String userToken = await db.fetchUserToken();
-
-    try {
-      final res = await http.post(
-          SubscriptionCreateURL,
-          body: {
-            'content': content,
-            'type': type,
-            'token': userToken,
-          }
-      );
-
-      if (res.statusCode == 201)
-        return true;
-      else
-        return false;
-    } catch (e) {
-      print(e);
-      return false;
-    }
   }
 
   Future<bool> delete() async {
-    final res = await http.delete(
-        SubscriptionDeleteURL + '/$id',
-    ).catchError((Error e) => false);
-    print(res.body);
-
-    if (res.statusCode == 200)
-      return true;
-    else
-      return false;
   }
 
   static Future<List<SubscriptionItem>> all() async{
-    DBManager db = new DBManager();
-    String userToken = await db.fetchUserToken();
-
-    final http.Response res = await http.get(
-      SubscriptionsURL + '/$userToken',
-    );
-
-    final List jsonArray = json.decode(res.body);
-    List<SubscriptionItem> subItems = [];
-
-    for(var subItem in jsonArray){
-      subItems.add(
-          new SubscriptionItem(
-              content: subItem['content'],
-              type: subItem['type'],
-              id: subItem['id'],
-          )
-      );
-    }
-
-    return subItems;
   }
 }
