28e357703fa00d0d206f2740e591fd063d32eeda
k-nasa <htilcs1115@gmail.com>
2018-07-15 12:03:02 +0900

    NewInfoの雛形を作成

---
+++ lib/model/new_info.dart
@@ -0,0 +1,13 @@
+class NewInfo {
+  // shared_preferencesのnew_infoを更新する
+  void updateNewInfo(bool new_info){
+  }
+
+  //shared_preferencesのnew_infoを返す
+  bool new_info() {
+  }
+
+  // new_infoをサーバーからもらう
+  void fetchNewInfo(){
+  }
+}
