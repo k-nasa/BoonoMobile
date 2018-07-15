362a29d5f07becdff2510599f0540120cabbc972
k-nasa <htilcs1115@gmail.com>
2018-07-15 13:41:50 +0900

    サーバーに情報が新鮮か問い合わせるメソッドを追加

--- lib/model/new_info.dart
+++ lib/model/new_info.dart
@@ -1,13 +1,31 @@
+import 'dart:async';
+
+import 'package:http/http.dart' as http;
+import 'package:boono_mobile/route/api_routes.dart';
+import 'package:boono_mobile/config/db_manager.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 class NewInfo {
   // shared_preferencesのnew_infoを更新する
-  void updateNewInfo(bool new_info){
+  static void updateNewInfo(bool new_info) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setBool('new_info', new_info);
   }
 
   //shared_preferencesのnew_infoを返す
-  bool new_info() {
+  static Future<bool> new_info() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getBool('new_info');
   }
 
   // new_infoをサーバーからもらう
-  void fetchNewInfo(){
+  static void  fetchNewInfo() async {
+    DBManager db = new DBManager();
+    final String userToken = await db.fetchUserToken();
+
+    var res = await http.get(NewInfoURL + '/$userToken');
+    bool new_info = res.body == 'true';
+
+    NewInfo.updateNewInfo(new_info);
   }
 }
--- lib/route/api_routes.dart
+++ lib/route/api_routes.dart
@@ -6,3 +6,4 @@ const String SubscriptionCreateURL = API_URL + '/subscription_items';
 const String SubscriptionDeleteURL = API_URL + '/subscription_items';
 const String SubscriptionsURL = API_URL + '/subscription_items';
 const String NotifyBookURL = API_URL + '/notify_books';
+const String NewInfoURL = API_URL + '/user_info';
