c9c846f2bc270789ae184b83887c760abe774188
k-nasa <htilcs1115@gmail.com>
2018-08-16 12:19:35 +0900

    delete/ 不要なStreamControllerを削除

--- lib/bloc/notify_book_bloc.dart
+++ lib/bloc/notify_book_bloc.dart
@@ -2,19 +2,7 @@ import 'dart:async';
 import 'package:rxdart/rxdart.dart';
 
 class NotifyBookBloc {
-
-  // TODO Stream, Sinkについて見直そう！
-  // listenerなしでも実装できるはず
-  NotifyBookBloc() {
-    _rebuildListViewController.stream.listen( (_) {
-      _rebuild.add(true);
-    });
-  }
-
-  final StreamController<bool> _rebuildListViewController = StreamController<bool>();
-  Sink<bool> get rebuildListView => _rebuildListViewController.sink;
-
   final BehaviorSubject<bool> _rebuild = BehaviorSubject<bool>(seedValue: true);
   Stream<bool> get isRebuildListView => _rebuild.stream;
-
+  Sink<bool> get rebuildListView => _rebuild.sink;
 }
