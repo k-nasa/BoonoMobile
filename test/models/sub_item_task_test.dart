f351ce1f561a2b8c993df7514ae62329ccee742d
k-nasa <htilcs1115@gmail.com>
2018-08-19 18:39:54 +0900

    add mock

--- test/models/sub_item_task_test.dart
+++ test/models/sub_item_task_test.dart
@@ -1,19 +1,39 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:sqflite/sqflite.dart';
 import 'package:boono_mobile/model/sub_item_task.dart';
+import 'package:flutter/services.dart';
+
+SubItemTask subItemTask = SubItemTask(
+  subId: 12,
+  httpMethod: 'get',
+  url: 'dummy_url',
+);
+
+Map subItemTaskMap = <String, dynamic> {
+  'http_method': 'get',
+  'url': 'dummy_url',
+  'sub_id': 12,
+};
+
 
 void main() {
   group('SubItemTask', () {
-    SubItemTask subItemTask = SubItemTask(
-      subId: 12,
-      httpMethod: 'get',
-      url: 'dummy_url',
-    );
+    const MethodChannel('com.tekartik.sqflite').setMockMethodCallHandler((MethodCall methodCall) async {
+      print(methodCall.method);
+      if (methodCall.method == 'openDatabase') {
+        Database db;
+        return db;
+      }
+      if(methodCall.method == 'query') {
+        return [subItemTaskMap];
+      }
 
-    Map subItemTaskMap = <String, dynamic> {
-      'http_method': 'get',
-      'url': 'dummy_url',
-      'sub_id': 12,
-    };
+      return null;
+    });
+
+    const MethodChannel('plugins.flutter.io/path_provider').setMockMethodCallHandler((MethodCall methodCall) async {
+      return '/dummy/path';
+    });
 
    test('toMap', (){
      expect(subItemTask.toMap(), subItemTaskMap);
