57ac6c878edf79ccd6e5bf0c4b3597ce1512338c
k-nasa <htilcs1115@gmail.com>
2018-08-19 20:00:41 +0900

    add shared_preferences mock

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,10 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:boono_mobile/main.dart' as app;
+import 'helper/shared_preferences_helper.dart';
 
-void main() {
+void main() async {
+  await prefsMock();
+
   testWidgets('app test', (WidgetTester tester) async {
     app.main();
     await tester.pumpWidget(new app.App());
