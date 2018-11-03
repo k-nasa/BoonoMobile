d1c68e8f915434a8ae90964aea7a239a905fb951
k-nasa <htilcs1115@gmail.com>
2018-11-03 10:46:58 +0900

    Delete comment

--- lib/main.dart
+++ lib/main.dart
@@ -15,11 +15,9 @@ void main() async {
   final Config config = Config();
   await NewInfo.fetchNewInfo();
 
-  // FIXME こういう判定もAppないでやって描画するScreenを変えるべき
   if (await config.init())
     runApp(App(currentTheme: currentTheme));
   else
-    // FIXME こういう判定もAppないでやって描画するScreenを変えるべき
     runApp(new ErrorPage());
 }
 
